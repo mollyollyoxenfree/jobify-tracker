a3527dec7c0a1af218efa24fa7c8ab983fa56466
Molly Smith <molly.ivester@gmail.com>
2023-08-13 21:29:30 -0400

    final commit

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -3,32 +3,49 @@ import { JobsContainer, SearchContainer } from '../components';
 import customFetch from '../utils/customFetch';
 import { useLoaderData } from 'react-router-dom';
 import { useContext, createContext } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
-export const loader = async ({ request }) => {
-  const params = Object.fromEntries([
-    ...new URL(request.url).searchParams.entries(),
-  ]);
-  //This code retrieves the URL from the request.url property,
-  //extracts the search parameters using the searchParams property,
-  //converts them into an array of key-value pairs using entries(),
-  //and finally uses Object.fromEntries() to create an object with the parameter names as keys and their corresponding values.
-  //The resulting object, params, contains all the search parameters from the URL.
-
-  try {
-    const { data } = await customFetch.get('/jobs', {
-      params,
-    });
-    return { data, searchValues: { ...params } };
-  } catch (error) {
-    toast.error(error?.response?.data?.msg);
-    return error;
-  }
+const allJobsQuery = (params) => {
+  const { search, jobStatus, jobType, sort, page } = params;
+  return {
+    queryKey: [
+      'jobs',
+      search ?? '',
+      jobStatus ?? 'all',
+      jobType ?? 'all',
+      sort ?? 'newest',
+      page ?? 1,
+    ],
+    queryFn: async () => {
+      const { data } = await customFetch.get('/jobs', {
+        params,
+      });
+      return data;
+    },
+  };
 };
+
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+    //This code retrieves the URL from the request.url property,
+    //extracts the search parameters using the searchParams property,
+    //converts them into an array of key-value pairs using entries(),
+    //and finally uses Object.fromEntries() to create an object with the parameter names as keys and their corresponding values.
+    //The resulting object, params, contains all the search parameters from the URL.
+
+    await queryClient.ensureQueryData(allJobsQuery(params));
+    return { searchValues: { ...params } };
+  };
 
 const AllJobsContext = createContext();
 
 const AllJobs = () => {
-  const { data, searchValues } = useLoaderData();
+  const { searchValues } = useLoaderData();
+  const { data } = useQuery(allJobsQuery(searchValues));
 
   return (
     <AllJobsContext.Provider value={{ data, searchValues }}>
